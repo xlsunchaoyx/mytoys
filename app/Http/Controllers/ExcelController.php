f8286e23fdf65d99faada2e5e36efcb407c69a6d
sun.chao <mr.sun.chao@foxmail.com>
2018-04-20 17:51:07 +0800

    调整导入模板顺序

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -25,10 +25,6 @@ class ExcelController extends Controller
 
         }
 
-
-
-
-
         $ext = $file_obj->getClientOriginalExtension();
         if (!in_array($ext, ['xls', 'xlsx'])) {
             $error = '请导入Excel格式文件！文件格式必须是xls、xlsx后缀的Excel格式的一种';
@@ -58,16 +54,15 @@ class ExcelController extends Controller
         for ($row = 2; $row <= $row_count; $row++) {
             // 验证解析的第一个和第二个字段，如果都为NULL或''，则认为到了解析边界
 
-            $create_date = $sheet->getCell('A' . $row)->getValue();
-            $order_id = $sheet->getCell('D' . $row)->getValue();
+            $order_id = $sheet->getCell('A' . $row)->getValue();
             $customer_name = $sheet->getCell('B' . $row)->getValue();
-            $phone = $sheet->getCell('K' . $row)->getValue();
-            $address = $sheet->getCell('H' . $row)->getValue();
-            $zip_code = $sheet->getCell('J' . $row)->getValue();
+            $phone = $sheet->getCell('C' . $row)->getValue();
+            $address = $sheet->getCell('D' . $row)->getValue();
+            $zip_code = $sheet->getCell('E' . $row)->getValue();
             $product_name = $sheet->getCell('F' . $row)->getValue();
             $product_name = trim($product_name);
             # 如果空数据 停止解析
-            if (!$create_date) {
+            if (!$order_id) {
                 break;
             }
 
@@ -80,7 +75,6 @@ class ExcelController extends Controller
                 $product_count = $data_array[1];
                 $new_product_name = implode('包邮 * ', [$data_array[0], '1']);
                 for ($x=1; $x<=$product_count; $x++) {
-                    ;
                     $data_dict = [
                         'order_id' => $order_id,
                         'customer_name' => $customer_name,
@@ -110,19 +104,29 @@ class ExcelController extends Controller
                 $sum = count($new_data);
                 $sheet->setAutoSize(true);
                 $sheet->setColumnFormat(array('E' => '@'));
-                $sheet->row(1, ['订单号', '客户', '手机', '地址', '邮编', '产品',]);
+                $sheet->row(1, ['订单号', '收货人', '收货手机', '收货详细地址(省 市 县 详细地址)', '收货地邮编', '货物描述', '数量', '备注', '物流单号', '物流订单号', '收货电话',]);
+
+                $order_prefix = date('YmdHis'); # 新订单号前缀
+                $count_len = strlen($sum); # 数字长度
 
                 $index = 0;
                 for ($i = 2; $i < $sum + 2; $i++) {
                     $obj = $new_data[$index];
 
+                    $new_order_name = str_pad($index+1, $count_len, '0', STR_PAD_LEFT);
+
                     $sheet->row($i, array(
-                        '订单号' => $obj['order_id'],
-                        '客户' => $obj['customer_name'],
-                        '手机' => $obj['phone'],
-                        '地址' => $obj['address'],
-                        '邮编' => $obj['zip_code'],
-                        '产品' => $obj['new_product_name'],
+                        '订单号' => $order_prefix . $new_order_name,
+                        '收货人' => $obj['customer_name'],
+                        '收货手机' => $obj['phone'],
+                        '收货详细地址(省 市 县 详细地址)' => $obj['address'],
+                        '收货地邮编' => $obj['zip_code'],
+                        '货物描述' => $obj['new_product_name'],
+                        '数量' => '',
+                        '备注' => $obj['order_id'],
+                        '物流单号' => '',
+                        '物流订单号' => '',
+                        '收货电话' => '',
                     ), $explicit = true);
                     $index = $index + 1;
                 }
